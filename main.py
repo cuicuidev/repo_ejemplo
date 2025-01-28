cef9fb46256ee99b43e0cc9f91d0fc8f806c1d71
Dmitry <dmitry.ryzhenkov@hackaboss.com>
2025-01-28 10:13:05 -0800

    iris

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import seaborn as sns
 
 def main():
-    """Esta función dice hola mundo."""
-    st.write("adiós mundo")
-    st.write("Es broma")
+    """Esta función muestra el dataframe del iris."""
+    df = sns.load_dataset("iris")
+    st.dataframe(df)
 
 if __name__ == "__main__":
     main()
